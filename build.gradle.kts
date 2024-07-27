e75eb9423e4c826390f7038cbc68f2da26195636
sanghee0820 <lsh277604@gmail.com>
2024-07-27 15:17:14 +0900

    build: lombok 관련 dependency 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -44,6 +44,10 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 //	runtimeOnly("com.mysql:mysql-connector-j")
 
+	// utils
+	compileOnly("org.projectlombok:lombok")
+	annotationProcessor("org.projectlombok:lombok")
+
 	// test
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
