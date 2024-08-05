d4c229f1aa444d6a6c12ceaf263f06e97d253317
sanghee0820 <lsh277604@gmail.com>
2024-08-05 15:47:05 +0900

    feat: API명세를 위한 Kakao OAuth Redirect API 추가

--- src/main/kotlin/bingsoochef/bingsoochef/auth/AuthController.kt
+++ src/main/kotlin/bingsoochef/bingsoochef/auth/AuthController.kt
@@ -3,13 +3,11 @@ package bingsoochef.bingsoochef.auth
 import bingsoochef.bingsoochef.presentation.req.LoginRequest
 import bingsoochef.bingsoochef.presentation.res.LoginResponse
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.headers.Header
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/users")
@@ -29,5 +27,18 @@ class AuthController {
             .body(response)
     }
 
-    
+    @Operation(summary = "Kakao OAuth")
+    @ApiResponses(
+        ApiResponse(
+            responseCode = "SEE OTHER",
+            headers = [Header(name = "Location", description = "Redirect URL")],
+            description = "Kakao OAuth로 Redirect"
+        )
+    )
+    @GetMapping("/oauth/kakao")
+    fun kakaoOauth(): ResponseEntity<Void> {
+        return ResponseEntity.status(303)
+            .header("Location", "")
+            .build()
+    }
 }
