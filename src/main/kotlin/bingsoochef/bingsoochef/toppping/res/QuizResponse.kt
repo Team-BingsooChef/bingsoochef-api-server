20f1188896b6fc7a467014db2082098ba3816a5c
HyeonJeong <wgwjh05169@gmail.com>
2024-08-05 18:10:25 +0900

    feat: 잘못된 DTO import 수정

--- src/main/kotlin/bingsoochef/bingsoochef/toppping/res/QuizResponse.kt
+++ src/main/kotlin/bingsoochef/bingsoochef/toppping/res/QuizResponse.kt
@@ -1,7 +1,5 @@
 package bingsoochef.bingsoochef.toppping.res
 
-import bingsoochef.bingsoochef.toppping.req.QuizDto
-
 data class QuizResponse(
     val quiz: QuizDto,
     val options: List<OptionDto>
