843fd53c9edc284c4ec9cdeea45253fe99007e84
HyeonJeong <wgwjh05169@gmail.com>
2024-08-05 18:02:59 +0900

    feat: 토핑 타입 조회 API 작성

---
+++ src/main/kotlin/bingsoochef/bingsoochef/toppping/res/ToppingTypeResponse.kt
@@ -0,0 +1,7 @@
+package bingsoochef.bingsoochef.toppping.res
+
+import bingsoochef.bingsoochef.toppping.domain.ToppingType
+
+data class ToppingTypeResponse(
+    val toppingTypes: List<ToppingType>
+)
