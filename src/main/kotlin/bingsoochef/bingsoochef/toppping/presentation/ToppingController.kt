9d45c6488ac9dcc92567b24b75be51c54c72e8bd
HyeonJeong <wgwjh05169@gmail.com>
2024-08-08 18:12:16 +0900

    docs: 토핑 생성 API에 대한 스웨거 Operation과 Api response 명세 작성

--- src/main/kotlin/bingsoochef/bingsoochef/toppping/presentation/ToppingController.kt
+++ src/main/kotlin/bingsoochef/bingsoochef/toppping/presentation/ToppingController.kt
@@ -6,6 +6,12 @@ import bingsoochef.bingsoochef.toppping.presentation.req.TryQuizRequest
 import bingsoochef.bingsoochef.toppping.presentation.res.QuizResponse
 import bingsoochef.bingsoochef.toppping.presentation.res.ToppingResponse
 import bingsoochef.bingsoochef.toppping.presentation.res.TryResultResponse
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.media.ArraySchema
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
@@ -15,6 +21,17 @@ import org.springframework.web.bind.annotation.*
 @RequestMapping("/users/bingsoos/toppings")
 class ToppingController {
 
+    @Operation(
+        summary = "토핑 생성 API",
+        description = "Request body 속 bingsoo id를 가진 빙수에 토핑을 생성합니다.<br>" +
+                "이때 주어진 access token 속 user id를 chef로 합니다."
+    )
+    @ApiResponses(value = [
+        ApiResponse(responseCode = "201", description = "생성된 토핑 반환",
+            content = [Content(mediaType = "application/json", array = (ArraySchema(schema = Schema(implementation = ToppingResponse::class))))]),
+        ApiResponse(responseCode = "400", description = "잘못된 QuizType을 요청함 / QuizType과 Option이 맞지 않음", content = [Content()]),
+        ApiResponse(responseCode = "404", description = "존재하지 않는 빙수임", content = [Content()])
+    ])
     @PostMapping
     fun createTopping(@RequestBody request: CreateToppingRequest): ResponseEntity<ToppingResponse> {
         TODO()
