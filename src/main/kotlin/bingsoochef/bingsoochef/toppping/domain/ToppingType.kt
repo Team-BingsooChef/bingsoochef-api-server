15f57bae845fb7f33ab7d1d8deac335f2e4ebca5
HyeonJeong <wgwjh05169@gmail.com>
2024-08-13 23:15:58 +0900

    refactor: ToppingType을 Entity 클래스로 수정

--- src/main/kotlin/bingsoochef/bingsoochef/toppping/domain/ToppingType.kt
+++ src/main/kotlin/bingsoochef/bingsoochef/toppping/domain/ToppingType.kt
@@ -1,7 +1,14 @@
 package bingsoochef.bingsoochef.toppping.domain
 
-data class ToppingType(
-    val toppingTypeId: Long,
+import jakarta.persistence.*
+
+@Table(name = "topping_type")
+@Entity
+class ToppingType(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "topping_type_id")
+    val id: Long,
+
     val toppingName: String,
     val toppingImg: String
 )
