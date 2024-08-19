ef71a3da76187d2143eea1277d0df7690a0a1a55
sanghee0820 <lsh277604@gmail.com>
2024-08-20 01:21:08 +0900

    feat: 이메일 인증 관련 Exception 추가

--- src/main/kotlin/bingsoochef/bingsoochef/common/exception/code/AuthError.kt
+++ src/main/kotlin/bingsoochef/bingsoochef/common/exception/code/AuthError.kt
@@ -8,7 +8,9 @@ enum class AuthError(
     val message: String
 ) : ErrorCode {
     EMAIL_FORMAT_INVALID(HttpStatus.BAD_REQUEST, "A001", "잘못된 이메일 형식"),
-    EMAIL_SEND_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "A002", "메일 발송 실패");
+    EMAIL_SEND_FAILED(HttpStatus.INTERNAL_SERVER_ERROR, "A002", "메일 발송 실패"),
+    EMAIL_CERTIFICATION_NOT_FOUND(HttpStatus.NOT_FOUND, "A003", "인증 정보를 찾을 수 없음"),
+    EMAIL_CERTIFICATION_FAILED(HttpStatus.BAD_REQUEST, "A004", "인증 실패, 인증을 다시 요청해주세요");
 
     override fun httpStatus(): HttpStatus {
         return httpStatus
