1d23a784b18d70bcf5203b8ffcfff07ba4788acc
HyeonJeong <wgwjh05169@gmail.com>
2024-08-20 21:06:14 +0900

    build: 로그 출력을 위한 kotlin logging 라이브러리 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,6 +35,7 @@ allOpen {
 dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("io.github.oshai:kotlin-logging-jvm:5.1.4")
 
 	// web
 	implementation("org.springframework.boot:spring-boot-starter-web")
