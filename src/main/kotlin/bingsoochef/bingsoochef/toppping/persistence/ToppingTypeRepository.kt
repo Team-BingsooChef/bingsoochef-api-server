ded4598a0c3c31f0ed50a1e467814d0a2a09b49a
HyeonJeong <wgwjh05169@gmail.com>
2024-08-20 21:21:53 +0900

    refactor: topping type repository에 스프링 Repository 애너테이션 추가

--- src/main/kotlin/bingsoochef/bingsoochef/toppping/persistence/ToppingTypeRepository.kt
+++ src/main/kotlin/bingsoochef/bingsoochef/toppping/persistence/ToppingTypeRepository.kt
@@ -2,6 +2,8 @@ package bingsoochef.bingsoochef.toppping.persistence
 
 import bingsoochef.bingsoochef.toppping.domain.ToppingType
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
+@Repository
 interface ToppingTypeRepository : JpaRepository<ToppingType, Long> {
 }
