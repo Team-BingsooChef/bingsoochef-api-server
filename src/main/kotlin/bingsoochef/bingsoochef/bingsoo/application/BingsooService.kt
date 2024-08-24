e5e6d7db4a50450d0ed358b01c05513e02d43625
HyeonJeong <wgwjh05169@gmail.com>
2024-08-25 00:46:43 +0900

    feat: 빙수 생성 서비스 구현

---
+++ src/main/kotlin/bingsoochef/bingsoochef/bingsoo/application/BingsooService.kt
@@ -0,0 +1,26 @@
+package bingsoochef.bingsoochef.bingsoo.application
+
+import bingsoochef.bingsoochef.bingsoo.domain.Bingsoo
+import bingsoochef.bingsoochef.bingsoo.persistence.BingsooRepository
+import bingsoochef.bingsoochef.global.error.NotFoundException
+import bingsoochef.bingsoochef.user.domain.User
+import bingsoochef.bingsoochef.user.persistence.UserRepository
+import jakarta.transaction.Transactional
+import org.springframework.stereotype.Service
+
+@Service
+@Transactional
+class BingsooService(
+    var userRepository: UserRepository,
+    var bingsooRepository: BingsooRepository) {
+
+    fun createBingsoo(command: CreateBingsooCommand): Bingsoo {
+        var user: User = userRepository.findById(command.userId)
+            .orElseThrow{ NotFoundException("존재하지 않는 사용자입니다.") }
+
+        val bingsoo: Bingsoo = bingsooRepository.save(Bingsoo(taste = command.taste))
+        // TODO("User에 Bingsoo 매핑")
+
+        return bingsoo
+    }
+}
