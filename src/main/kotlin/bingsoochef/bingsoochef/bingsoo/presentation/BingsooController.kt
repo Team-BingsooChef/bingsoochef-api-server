048d069ae6b7d83c69f4ed25c58874454083455a
HyeonJeong <wgwjh05169@gmail.com>
2024-08-25 00:48:02 +0900

    feat: 빙수 생성 Rest controller 구현

--- src/main/kotlin/bingsoochef/bingsoochef/bingsoo/presentation/BingsooController.kt
+++ src/main/kotlin/bingsoochef/bingsoochef/bingsoo/presentation/BingsooController.kt
@@ -1,19 +1,37 @@
 package bingsoochef.bingsoochef.bingsoo.presentation
 
+import bingsoochef.bingsoochef.bingsoo.application.BingsooService
+import bingsoochef.bingsoochef.bingsoo.application.CreateBingsooCommand
 import bingsoochef.bingsoochef.bingsoo.presentation.req.CreateBingsooRequest
 import bingsoochef.bingsoochef.bingsoo.presentation.req.UpdateBingsooRequest
 import bingsoochef.bingsoochef.bingsoo.presentation.res.BingsooDetailResponse
+import bingsoochef.bingsoochef.bingsoo.presentation.res.BingsooDto
 import bingsoochef.bingsoochef.bingsoo.presentation.res.BingsooResponse
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController()
 @RequestMapping("/users/bingsoo")
-class BingsooController : BingsooControllerInterface {
+class BingsooController(
+    private val bingsooService : BingsooService
+): BingsooControllerInterface {
 
     @PostMapping
-    override fun createBingsoo(@RequestBody request: CreateBingsooRequest): ResponseEntity<BingsooResponse> {
-        TODO()
+    override fun createBingsoo(@RequestBody request: CreateBingsooRequest) : ResponseEntity<BingsooResponse> {
+        // TODO("user id를 access token에서 가져오도록 수정")
+        val userId = 1L
+
+        val createCommand = CreateBingsooCommand(
+            userId,
+            request.taste
+        )
+
+        val bingsoo = bingsooService.createBingsoo(createCommand)
+        val bingsooDto = BingsooDto.from(bingsoo)
+        val bingsooResponse = BingsooResponse.of(bingsooDto)
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(bingsooResponse)
     }
 
     @GetMapping("/{bingsoo-id}")
