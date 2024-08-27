be476fd154cd2faf537dc6c5660cd9fd6a19d915
HyeonJeong <wgwjh05169@gmail.com>
2024-08-28 02:15:09 +0900

    refactor: 토핑 목록 조회 API의 응답에 전체 항목 개수 추가

--- src/main/kotlin/bingsoochef/bingsoochef/toppping/presentation/res/ToppingPageResponse.kt
+++ src/main/kotlin/bingsoochef/bingsoochef/toppping/presentation/res/ToppingPageResponse.kt
@@ -3,5 +3,6 @@ package bingsoochef.bingsoochef.toppping.presentation.res
 data class ToppingPageResponse(
     val currPage: Int,
     val totalPage: Int,
+    val totalElements: Int,
     val toppings: List<ToppingAbstractDto>
 )
