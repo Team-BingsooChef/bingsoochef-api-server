4dd9b8dc54f85602065156804197a8d332b027b9
HyeonJeong <wgwjh05169@gmail.com>
2024-08-29 14:21:00 +0900

    Refactor: JSON 생성에 문제가 있을 경우 IllegalArgumentException이 발생하도록 수정

--- src/main/kotlin/bingsoochef/bingsoochef/bingsoo/domain/Taste.kt
+++ src/main/kotlin/bingsoochef/bingsoochef/bingsoo/domain/Taste.kt
@@ -1,5 +1,16 @@
 package bingsoochef.bingsoochef.bingsoo.domain
 
+import com.fasterxml.jackson.annotation.JsonCreator
+
 enum class Taste {
-    STRAWBERRY, CHOCO, CONDENSED_MILK, MATCHA, MANGO, MINT_CHOCO
+    STRAWBERRY, CHOCO, CONDENSED_MILK, MATCHA, MANGO, MINT_CHOCO;
+
+    companion object {
+        @JsonCreator
+        @JvmStatic
+        fun fromValue(value: String): Taste {
+            return entries.find { it.name.equals(value, ignoreCase = true) }
+                ?: throw IllegalArgumentException("'$value'는 '$entries' 중에 존재하지 않습니다.")
+        }
+    }
 }
