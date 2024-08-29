3f53c8e32ea44bab226df01bb202cece1a97a4c4
HyeonJeong <wgwjh05169@gmail.com>
2024-08-29 22:54:56 +0900

    chore: 제거한 기능 삭제

--- src/main/kotlin/bingsoochef/bingsoochef/bingsoo/domain/Taste.kt
+++ src/main/kotlin/bingsoochef/bingsoochef/bingsoo/domain/Taste.kt
@@ -1,16 +1,6 @@
 package bingsoochef.bingsoochef.bingsoo.domain
 
-import com.fasterxml.jackson.annotation.JsonCreator
-
 enum class Taste {
     STRAWBERRY, CHOCO, CONDENSED_MILK, MATCHA, MANGO, MINT_CHOCO;
 
-//    companion object {
-//        @JsonCreator
-//        @JvmStatic
-//        fun fromValue(value: String): Taste {
-//            return entries.find { it.name.equals(value, ignoreCase = true) }
-//                ?: throw IllegalArgumentException("'$value'는 '$entries' 중에 존재하지 않습니다.")
-//        }
-//    }
 }
