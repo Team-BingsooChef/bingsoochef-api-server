177733c9acc261552e9de3fd375d5881f3ab1377
HyeonJeong <wgwjh05169@gmail.com>
2024-09-03 16:45:19 +0900

    refactor: 요청 API에 임시 사용자 ID 추가

--- src/main/kotlin/bingsoochef/bingsoochef/toppping/presentation/req/CreateToppingRequest.kt
+++ src/main/kotlin/bingsoochef/bingsoochef/toppping/presentation/req/CreateToppingRequest.kt
@@ -1,6 +1,7 @@
 package bingsoochef.bingsoochef.toppping.presentation.req
 
 data class CreateToppingRequest (
+    val userId: Long,    // 로그인 구현 전까지 사용할 임시 필드
     val bingsooId: Long,
     val topping: ToppingDto,
     val quiz: QuizDto
