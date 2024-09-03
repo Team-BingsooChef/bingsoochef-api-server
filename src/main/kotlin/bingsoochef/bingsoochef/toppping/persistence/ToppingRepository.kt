ba7c4b7501f14f965986155f4b6003ca8bc19830
HyeonJeong <wgwjh05169@gmail.com>
2024-09-03 16:48:14 +0900

    feat: 빙수에 달린 토핑 개수를 세는 메소드 작성

--- src/main/kotlin/bingsoochef/bingsoochef/toppping/persistence/ToppingRepository.kt
+++ src/main/kotlin/bingsoochef/bingsoochef/toppping/persistence/ToppingRepository.kt
@@ -1,9 +1,12 @@
 package bingsoochef.bingsoochef.toppping.persistence
 
+import bingsoochef.bingsoochef.bingsoo.domain.Bingsoo
 import bingsoochef.bingsoochef.toppping.domain.Topping
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface ToppingRepository : JpaRepository<Topping, Long> {
+
+    fun countByBingsooIs(bingsoo: Bingsoo): Long
 }
