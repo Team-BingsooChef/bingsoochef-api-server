7087784e024ca72d512a191d0f3ae0c33d6d8837
HyeonJeong <wgwjh05169@gmail.com>
2024-09-03 16:49:00 +0900

    refactor: 토핑 응답 DTO에 from 메소드 작성

--- src/main/kotlin/bingsoochef/bingsoochef/toppping/presentation/res/ToppingDto.kt
+++ src/main/kotlin/bingsoochef/bingsoochef/toppping/presentation/res/ToppingDto.kt
@@ -1,5 +1,6 @@
 package bingsoochef.bingsoochef.toppping.presentation.res
 
+import bingsoochef.bingsoochef.toppping.domain.Topping
 import java.time.LocalDateTime
 
 data class ToppingDto (
@@ -12,4 +13,21 @@ data class ToppingDto (
     val toppingContent: String,
     val toppingPosition: Long,
     val toppingCreatedTime: LocalDateTime
-)
+) {
+
+    companion object {
+        fun from(topping : Topping): ToppingDto {
+            return ToppingDto(
+                toppingId = topping.id!!,
+                chefId = topping.chef.userId!!,
+                bingsooId = topping.bingsoo.id!!,
+                toppingTypeId = topping.toppingType.id!!,
+                chefName = topping.chefName,
+                toppingTitle = topping.title,
+                toppingContent = topping.content,
+                toppingPosition = topping.position,
+                toppingCreatedTime = topping.createdTime
+            )
+        }
+    }
+}
