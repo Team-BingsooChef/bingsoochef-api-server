d08972194b5868d3ba68894a424fe84386c6d368
HyeonJeong <wgwjh05169@gmail.com>
2024-09-03 16:49:16 +0900

    feat: 토핑 생성 API 웹 컨트롤러 작성

--- src/main/kotlin/bingsoochef/bingsoochef/toppping/presentation/ToppingController.kt
+++ src/main/kotlin/bingsoochef/bingsoochef/toppping/presentation/ToppingController.kt
@@ -1,24 +1,45 @@
 package bingsoochef.bingsoochef.toppping.presentation
 
+import bingsoochef.bingsoochef.toppping.application.CreateToppingCommand
+import bingsoochef.bingsoochef.toppping.application.ToppingService
+import bingsoochef.bingsoochef.toppping.domain.Topping
 import bingsoochef.bingsoochef.toppping.presentation.req.CreateToppingRequest
 import bingsoochef.bingsoochef.toppping.presentation.req.RegisterCommentRequest
 import bingsoochef.bingsoochef.toppping.presentation.req.TryQuizRequest
-import bingsoochef.bingsoochef.toppping.presentation.res.QuizResponse
-import bingsoochef.bingsoochef.toppping.presentation.res.ToppingPageResponse
-import bingsoochef.bingsoochef.toppping.presentation.res.ToppingResponse
-import bingsoochef.bingsoochef.toppping.presentation.res.TryResultResponse
+import bingsoochef.bingsoochef.toppping.presentation.res.*
 import org.springframework.data.domain.Pageable
 import org.springframework.data.web.PageableDefault
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController()
 @RequestMapping("/users/bingsoos/toppings")
-class ToppingController : ToppingControllerInterface {
+class ToppingController(
+    private val toppingService: ToppingService
+) : ToppingControllerInterface {
 
     @PostMapping
     override fun createTopping(@RequestBody request: CreateToppingRequest): ResponseEntity<ToppingResponse> {
-        TODO()
+        val command =
+            CreateToppingCommand(
+                bingsooId = request.bingsooId,
+                chefId = request.userId,
+                chefName = request.topping.chefName,
+                toppingTitle = request.topping.toppingTitle,
+                toppingContent = request.topping.toppingContent,
+                toppingTypeId = request.toppingTypeId,
+                quizType = request.quiz?.quizType,
+                quizTitle = request.quiz?.quizTitle,
+                questions = request.quiz?.questions
+            )
+
+        val topping : Topping = toppingService.createTopping(command)
+
+        val toppingDto = ToppingDto.from(topping)
+        val toppingResponse = ToppingResponse(topping = toppingDto, comment = null)
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(toppingResponse)
     }
 
     @GetMapping("")
