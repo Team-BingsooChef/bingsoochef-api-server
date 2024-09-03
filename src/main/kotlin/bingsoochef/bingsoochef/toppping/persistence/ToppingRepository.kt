2d37f0366e6349054e52cc324f038267bec26a74
HyeonJeong <wgwjh05169@gmail.com>
2024-09-03 20:33:32 +0900

    feat: 빙수와 셰프를 가진 토핑이 존재하는지 검사하는 메소드 작성

--- src/main/kotlin/bingsoochef/bingsoochef/toppping/persistence/ToppingRepository.kt
+++ src/main/kotlin/bingsoochef/bingsoochef/toppping/persistence/ToppingRepository.kt
@@ -2,11 +2,14 @@ package bingsoochef.bingsoochef.toppping.persistence
 
 import bingsoochef.bingsoochef.bingsoo.domain.Bingsoo
 import bingsoochef.bingsoochef.toppping.domain.Topping
+import bingsoochef.bingsoochef.user.domain.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface ToppingRepository : JpaRepository<Topping, Long> {
+
+    fun existsByBingsooAndChef(bingsoo: Bingsoo, chef: User): Boolean
 
     fun countByBingsooIs(bingsoo: Bingsoo): Long
 }
