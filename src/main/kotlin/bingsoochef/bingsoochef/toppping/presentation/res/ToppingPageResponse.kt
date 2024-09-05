e0fcbfa13e59d0f474a260542cb4451613c7be6a
HyeonJeong <wgwjh05169@gmail.com>
2024-09-05 16:26:40 +0900

    refactor: 토핑페이지 조회 응답 DTO에 from 메소드 작성

--- src/main/kotlin/bingsoochef/bingsoochef/toppping/presentation/res/ToppingPageResponse.kt
+++ src/main/kotlin/bingsoochef/bingsoochef/toppping/presentation/res/ToppingPageResponse.kt
@@ -1,9 +1,11 @@
 package bingsoochef.bingsoochef.toppping.presentation.res
 
+import bingsoochef.bingsoochef.toppping.application.dto.ToppingPageInfo
+
 data class ToppingPageResponse(
     val currPage: Int,
-    val totalPage: Int,
-    val totalElements: Int,
+    val totalPages: Int,
+    val totalElements: Long,
     val toppings: List<ToppingDto>
 ) {
     class ToppingDto(
@@ -13,4 +15,21 @@ data class ToppingPageResponse(
         val toppingPosition: Long,
         val isHidden: Boolean
     )
+
+    companion object {
+        fun from(toppingPageInfo: ToppingPageInfo): ToppingPageResponse {
+            return ToppingPageResponse(
+                currPage = toppingPageInfo.currPage,
+                totalPages = toppingPageInfo.totalPages,
+                totalElements = toppingPageInfo.totalElements,
+                toppings = toppingPageInfo.toppings.map { ToppingDto(
+                    toppingId = it.id,
+                    toppingTypeId = it.toppingTypeId,
+                    toppingTitle = it.title,
+                    toppingPosition = it.position,
+                    isHidden = it.isHidden
+                ) }
+            )
+        }
+    }
 }
