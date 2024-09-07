c03a59394f2b91c29a18ff1baad73a4355bcd496
HyeonJeong <wgwjh05169@gmail.com>
2024-09-07 21:17:05 +0900

    test: topping의 상태를 확인하는 조건 추가

--- src/test/kotlin/bingsoochef/bingsoochef/topping/application/ToppingAccessTest.kt
+++ src/test/kotlin/bingsoochef/bingsoochef/topping/application/ToppingAccessTest.kt
@@ -9,6 +9,7 @@ import bingsoochef.bingsoochef.user.domain.User
 import io.kotest.assertions.throwables.shouldNotThrow
 import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.core.spec.style.FunSpec
+import io.kotest.matchers.shouldBe
 import io.kotest.matchers.string.shouldContain
 import java.time.LocalDateTime
 
@@ -43,6 +44,11 @@ class ToppingAccessTest : FunSpec({
     }
 
     context("빙수에 얼어 있는 토핑이 있을 때") {
+
+        test("빙수가 얼어 있다.") {
+            topping.isHidden shouldBe true
+        }
+
         test("손님이 접근하면 403 - 빙수가 얼어 있음 예외가 발생한다") {
             val exception = shouldThrow<ForbiddenException> {
                 topping.isReadableBy(customer)
@@ -66,6 +72,10 @@ class ToppingAccessTest : FunSpec({
 
     context("빙수에 녹은 토핑이 있을 때") {
         topping.defrost()
+
+        test("빙수가 녹아 있다.") {
+            topping.isHidden shouldBe false
+        }
 
         test("손님은 접근할 수 있다.") {
             shouldNotThrow<ForbiddenException> {
