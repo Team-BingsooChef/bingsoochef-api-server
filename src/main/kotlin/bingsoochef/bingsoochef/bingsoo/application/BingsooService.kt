f1ffd21e099834e987b04dca4e4a8be3d448c9de
sanghee0820 <lsh277604@gmail.com>
2024-09-07 23:11:22 +0900

    refactor: bingsoo 정보의 nullable에 따른 null확인 추가

--- src/main/kotlin/bingsoochef/bingsoochef/bingsoo/application/BingsooService.kt
+++ src/main/kotlin/bingsoochef/bingsoochef/bingsoo/application/BingsooService.kt
@@ -17,7 +17,7 @@ class BingsooService(
 
     fun createBingsoo(command: CreateBingsooCommand): Bingsoo {
         var user: User = userRepository.findById(command.userId)
-            .orElseThrow{ NotFoundException("존재하지 않는 사용자입니다.") }
+            .orElseThrow { NotFoundException("존재하지 않는 사용자입니다.") }
 
         val bingsoo: Bingsoo = bingsooRepository.save(Bingsoo(taste = command.taste))
         // TODO("User에 Bingsoo 매핑")
@@ -27,8 +27,8 @@ class BingsooService(
 
     fun updateBingsoo(command: UpdateBingsooCommand): Bingsoo {
         val bingsoo = userRepository.findById(command.userId)
-            .orElseThrow{ NotFoundException("존재하지 않는 사용자입니다.") }
-            .bingsoo
+            .orElseThrow { NotFoundException("존재하지 않는 사용자입니다.") }
+            .bingsoo ?: throw NotFoundException("존재하지 않는 빙수입니다.")
 
         bingsoo.updateTaste(taste = command.taste)
 
@@ -38,6 +38,6 @@ class BingsooService(
     @Transactional(readOnly = true)
     fun getBingsoo(bingsooId: Long): Bingsoo {
         return bingsooRepository.findById(bingsooId)
-            .orElseThrow{ NotFoundException("존재하지 않는 빙수입니다.") }
+            .orElseThrow { NotFoundException("존재하지 않는 빙수입니다.") }
     }
 }
