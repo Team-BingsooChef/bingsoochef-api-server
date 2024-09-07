98f1395dc3bafbfdfac2ad087ac906a2a1dab506
HyeonJeong <wgwjh05169@gmail.com>
2024-09-07 19:22:14 +0900

    feat: CommentInfo에 from 메소드 작성

--- src/main/kotlin/bingsoochef/bingsoochef/toppping/application/dto/CommentInfo.kt
+++ src/main/kotlin/bingsoochef/bingsoochef/toppping/application/dto/CommentInfo.kt
@@ -1,9 +1,20 @@
 package bingsoochef.bingsoochef.toppping.application.dto
 
+import bingsoochef.bingsoochef.toppping.domain.Comment
 import java.time.LocalDateTime
 
 data class CommentInfo (
     val id: Long,
     val content: String,
     val createdTime: LocalDateTime
-)
+) {
+    companion object {
+        fun from(comment: Comment): CommentInfo {
+            return CommentInfo(
+                id = comment.id!!,
+                content = comment.content,
+                createdTime = comment.createdTime
+            )
+        }
+    }
+}
