6feeee4f85bb250bf87911f8b0447c2db9dbe2a7
sanghee0820 <lsh277604@gmail.com>
2024-09-12 11:04:11 +0900

    feat: Merge충돌로 삭제된 User 기본정보 및 nullable 추가

--- src/main/kotlin/bingsoochef/bingsoochef/user/domain/User.kt
+++ src/main/kotlin/bingsoochef/bingsoochef/user/domain/User.kt
@@ -24,5 +24,14 @@ class User(
 
     @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "bingsoo_id", unique = true)
-    var bingsoo: Bingsoo
-)
+    var bingsoo: Bingsoo? = null
+
+) {
+    constructor(username: String, password: String, userType: AccountType) : this(
+        null,
+        username,
+        password,
+        null,
+        userType
+    )
+}
