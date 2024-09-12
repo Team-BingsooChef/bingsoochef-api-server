4124597282e65bdf636caa611396365c92feeebb
sanghee0820 <lsh277604@gmail.com>
2024-09-12 10:59:01 +0900

    refactor: NotNull을 ServletRequestBindingException으로 핸들링 하도록 변경

--- src/main/kotlin/bingsoochef/bingsoochef/security/presentation/req/EmailRequest.kt
+++ src/main/kotlin/bingsoochef/bingsoochef/security/presentation/req/EmailRequest.kt
@@ -1,11 +1,9 @@
 package bingsoochef.bingsoochef.security.presentation.req
 
 import jakarta.validation.constraints.Email
-import jakarta.validation.constraints.NotNull
 
 class EmailRequest(
     @Email
-    @NotNull
     val email: String
 ) {
 }
