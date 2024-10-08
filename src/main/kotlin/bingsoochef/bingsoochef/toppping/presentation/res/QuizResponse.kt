c84b01e0aa0a52bb05fe4e1f7b0c07624f0296ef
HyeonJeong <wgwjh05169@gmail.com>
2024-10-08 15:18:10 +0900

    feat: 퀴즈 응답 Inner class에 형변환 메소드 작성

--- src/main/kotlin/bingsoochef/bingsoochef/toppping/presentation/res/QuizResponse.kt
+++ src/main/kotlin/bingsoochef/bingsoochef/toppping/presentation/res/QuizResponse.kt
@@ -1,5 +1,6 @@
 package bingsoochef.bingsoochef.toppping.presentation.res
 
+import bingsoochef.bingsoochef.toppping.application.dto.QuizInfo
 import bingsoochef.bingsoochef.toppping.domain.QuizType
 
 data class QuizResponse(
@@ -8,13 +9,36 @@ data class QuizResponse(
 ) {
     class QuizDto(
         val quizId: Long,
-        val quizTitle: String,
         val quizType: QuizType,
+        val quizTitle: String,
         val wrongCount: Short
     )
 
     class QuestionDto(
         val questionId: Long,
         val questionContent: String
-    )
+    ) {
+        companion object {
+            fun from(info: QuizInfo.QuestionInfo): QuestionDto {
+                return QuestionDto(
+                    questionId = info.questionid,
+                    questionContent = info.content
+                )
+            }
+        }
+    }
+
+    companion object {
+        fun from(info: QuizInfo): QuizResponse {
+            return QuizResponse(
+                quiz = QuizDto(
+                    quizId = info.quizId,
+                    quizType = info.quizType,
+                    quizTitle = info.title,
+                    wrongCount = info.wrongCount
+                ),
+                questions = info.questions.map { QuestionDto.from(it) }
+            )
+        }
+    }
 }
